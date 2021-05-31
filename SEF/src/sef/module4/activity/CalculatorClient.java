8f0a650508a69f80599d1a5721ba7b9683a8b769
jelenas <leno4ka123>
2021-05-31 23:09:32 +0300

    Complete homework in Calculator.java and CalculatorClient.java

--- SEF/src/sef/module4/activity/CalculatorClient.java
+++ SEF/src/sef/module4/activity/CalculatorClient.java
@@ -21,17 +21,18 @@ public class CalculatorClient {
 		// Add 
 		int result = normalCalc.add(4, 5);
 		System.out.println("1) Add result is " + result);
-		
-				
+
 		// Multiply 
 		int[] mul = {2, 3, 4};
 		result = normalCalc.multiply(mul);
-		System.out.println("2) Multiplication result is " + result); 
-		
+        System.out.println("2) Multiplication result is " + result);
 
-	}
+        //// Divide
+        result = normalCalc.divide (4, 5);
+        System.out.println("3) Subtract result is " + result);
 
-
-	
-
+        //Subtract
+        result = normalCalc.subtract (5, 4);
+        System.out.println("4) Subtract result is " + result);
+        }
 }
